c868ecbf3bc8f8b88ec283078d1fbe277befff9d
Mohitesh Kumar <mohiteshk@gmail.com>
2026-01-11 10:40:40 +0530

    Adding rocketmouse data

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject targetObject;
+	private float distanceToTarget;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float targetObjectX = targetObject.transform.position.x;
+		Vector3 newCameraPosition = transform.position;
+		newCameraPosition.x = targetObjectX + distanceToTarget;
+		transform.position = newCameraPosition;
+	}
+}
---
+++ Assets/Scripts/SortingLayerFlamesFix.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SortingLayerFlamesFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "Player";
+		particleSystem.renderer.sortingOrder = -1;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
